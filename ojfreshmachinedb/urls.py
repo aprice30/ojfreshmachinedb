c5f509b152a303750a28a904ab4a24e9479a2afb
Adam Price <aprice30@gmail.com>
2015-03-21 12:52:58 +0000

    Changed the admin site title

--- ojfreshmachinedb/urls.py
+++ ojfreshmachinedb/urls.py
@@ -10,3 +10,13 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls))
 )
+
+
+# Text to put at the end of each page's <title>.
+admin.site.site_title = 'OJ Fresh Admin'
+
+# Text to put in each page's <h1>.
+admin.site.site_header = 'OJ Fresh Database'
+
+# Text to put at the top of the admin index page.
+admin.site.index_title = 'Admin'
